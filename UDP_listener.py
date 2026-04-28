720171d6d1adf90cd64eac15508883e2e7e5636b
JSETH159 <joshuaethanseth@gmail.com>
2026-04-28 17:02:29 +0100

    Delete UDP_listener.py

--- UDP_listener.py
+++
@@ -1,6 +0,0 @@
-import socket
-listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-listener.bind(("0.0.0.0", 9999))
-print("UDP listener ready on port 9999")
-while True:
-    listener.recv(1024)  # silently absorb incoming packets
